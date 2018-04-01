6c9642a87adf79045e78fdcbcb353d43fae6f857
RevLow <rev_low@yahoo.co.jp>
2018-04-01 16:24:37 +0900

    fix pchファイルの定義をmacro.hとLiveEngine.hに分離
    fix pchファイルの定義をmacro.hとLiveEngine.hに分離
    
    feat LiveEngine.hを追加

--- Library/LiveEngine/include/LiveEngine.h
+++ Library/LiveEngine/include/LiveEngine.h
@@ -6,3 +6,24 @@
 //  Copyright (c) 2018年 RevLow. All rights reserved.
 //
 
+#ifndef __LiveEngine__LiveEngine__
+#define __LiveEngine__LiveEngine__
+
+#include <memory>
+#include <vector>
+#include <array>
+#include <deque>
+#include <stack>
+#include <unordered_map>
+#include <iostream>
+#include <math.h>
+#include <cassert>
+#include <OpenGLES/ES3/gl.h>
+#include <OpenGLES/ES3/glext.h>
+
+#include "../base/macro.h"
+#include "../platform/Application.h"
+#include "../2d/Sprite.h"
+#include "../controller/NodeController.h"
+
+#endif
