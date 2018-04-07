7174186d9ddbcb31cd71b8e9f3d331995298e33e
RevLow <rev_low@yahoo.co.jp>
2018-04-07 11:37:34 +0900

    feat レンダリングの処理の流れを修正

--- Library/LiveEngine/task/RenderTask.cpp
+++ Library/LiveEngine/task/RenderTask.cpp
@@ -7,68 +7,85 @@
 //
 
 #include "RenderTask.h"
-#include "../render/VertexBuffer.h"
 
 using namespace live;
 
-void RenderTask::run() const
+RenderTask::RenderTask()
+: vao(std::unique_ptr<VAO>(new VAO()))
+, filledVertex(0)
+, filledIndex(0)
+{}
+
 void RenderTask::run()
 {
     const RenderGroup& renderGroup = Application::getInstance()->getRenderGroup();
-    
+    std::vector<TriangleRenderCommandPtrReference> batchedCommand;
     for (const std::unique_ptr<RenderQueue>& renderQueue : renderGroup )
     {
-        const int vertSize = 3 * 2 * renderQueue->queue.size();
-        const int bufferSize = vertSize * 9;
-        
-        // render頂点バッファを作成
-        GLfloat* buffer = (GLfloat*)malloc(sizeof(GLfloat) * bufferSize);
-        int bufferCount = 0;
-        for (const std::unique_ptr<TriangleRenderCommand>& cmd : renderQueue->queue)
+        for(const std::unique_ptr<TriangleRenderCommand>& cmd : renderQueue->queue)
         {
-            for(const Triangle& triangle : cmd->getTriangles())
-            {
-                for(const Vertex& vertex : triangle.vertices)
-                {
-                    buffer[bufferCount + 0] = vertex.position.x;
-                    buffer[bufferCount + 1] = vertex.position.y;
-                    buffer[bufferCount + 2] = vertex.position.z;
-                    
-                    buffer[bufferCount + 3] = vertex.color.x;
-                    buffer[bufferCount + 4] = vertex.color.y;
-                    buffer[bufferCount + 5] = vertex.color.z;
-                    buffer[bufferCount + 6] = vertex.color.w;
-
-                    buffer[bufferCount + 7] = vertex.uv.u;
-                    buffer[bufferCount + 8] = vertex.uv.v;
-
-                    bufferCount += 9;
-                }
-            }
+            fillVerticesAndIndices(cmd);
+            batchedCommand.emplace_back(cmd);
         }
-
-        // renderQueue.materialIdからシェーダを使用する
-        renderQueue->material->use();
-
-        /*
-         Shader shader("vertexshaderのファイル名", "fragmentshaderのファイル名");
-         shader.compile();
-         shader.useProgram();
-        */
-        
-
-        /*
-         VBO vbo(buffer, vertSize);
-         free(buffer);
-
-         VAO vao(vbo);
-         vao.draw();
-        */
-        
-        
-
-        // vbo, vao delete
-        // release vertex buffer and vertex array
     }
 
+    rendering(batchedCommand);
+    clean();
+}
+
+void RenderTask::fillVerticesAndIndices(const std::unique_ptr<TriangleRenderCommand>& cmd)
+{
+    Vertex* vptr = vao->getVbo()->getVertsPtr();
+    memcpy(vptr + filledVertex,
+           cmd->getVertices(),
+           sizeof(Vertex) * TriangleRenderCommand::VerticesCount());
+
+    for(ssize_t i = 0; i < TriangleRenderCommand::VerticesCount(); i++)
+    {
+        Vertex* vertex = &vptr[i + filledVertex];
+        Vec3* position = &vertex->position;
+
+        // new_positionは一時的な変数なので右辺値参照型で受け取り、
+        // positionの各要素に代入していく
+        Vec4&& new_position = cmd->getModelMatrix() * *position;
+        position->x = new_position.x;
+        position->y = new_position.y;
+        position->z = new_position.z;
+    }
+
+    const unsigned short* indices = cmd->getIndices();
+    GLushort* indicesPtr = vao->getVbo()->getIndicesPtr();
+    for (ssize_t i = 0; i < TriangleRenderCommand::IndexCount(); i++)
+    {
+        indicesPtr[i + filledIndex] = filledVertex + indices[i];
+    }
+
+    filledVertex += TriangleRenderCommand::VerticesCount();
+    filledIndex += TriangleRenderCommand::IndexCount();
+}
+
+void RenderTask::rendering(std::vector<TriangleRenderCommandPtrReference>& batchedCommand)
+{
+    uint32_t lastMaterialId = 0;
+    ssize_t indexCount = 0, startIndex = 0;
+    // 貯めていた頂点バッファをGPUに転送
+    vao->bind(filledVertex, filledIndex);
+
+    for(std::unique_ptr<TriangleRenderCommand>& cmd : batchedCommand)
+    {
+        indexCount += cmd->IndexCount();
+        
+        // 最後のマテリアルIDと違うなら、マテリアルを有効化
+        // cmd->useMaterial();
+        // vao->draw(indexCount, startIndex);
+    }
+
+    vao->unbind();
+}
+
+
+void RenderTask::clean()
+{
+    filledVertex = 0;
+    filledIndex = 0;
 }
--- Library/LiveEngine/task/RenderTask.h
+++ Library/LiveEngine/task/RenderTask.h
@@ -10,14 +10,23 @@
 #define __LiveEngine__RenderTask__
 
 #include "ITask.h"
+#include "VertexBuffer.h"
 
 namespace live
 {
     class RenderTask : public ITask
     {
-      public:
-        virtual void run() const;
+        using TriangleRenderCommandPtrReference = std::reference_wrapper<std::unique_ptr<TriangleRenderCommand>>;
+    public:
+        RenderTask();
         virtual void run();
+    private:
+        void fillVerticesAndIndices(const std::unique_ptr<TriangleRenderCommand>& cmd);
+        void rendering(std::vector<TriangleRenderCommandPtrReference>& batchedCommand);
+        void clean();
+        std::unique_ptr<VAO> vao;
+        int filledVertex;
+        int filledIndex;
     };
 }
 
